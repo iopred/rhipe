fe1441858fd464a01303aedfacf4d28d265bc12b
Chris Rhodes <iopred@gmail.com>
2013-01-04 18:52:38 -0800

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 cf.difficulty = [0.2, 0.4, 0.6, 0.8, 1]
-cf.difficultyName = ["Amoeba", "Human", "Hero", "Superhero", "Godlike"]
+cf.difficultyName = ["Amoeba", "Human", "Hero", "Superhero", "Demigod"]
 cf.exercises = [
 	{"group": "row", "bumper": false, "weight": 1, "not-reps": false, "maxweight": 0, "name": "Reverse Pushups", "grp": 1, "maximum": 80, "multiple": 5},
 	{"group": "snatch", "bumper": false, "weight": .25, "not-reps": false, "maxweight": 55, "name": "Kettle Bell Snatch (per arm)", "grp": 1, "maximum": 50, "multiple": 5},
